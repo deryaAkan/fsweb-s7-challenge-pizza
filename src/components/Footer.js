b10a2260a60e1d3f1c24dd90b22a9edff5b7f035
Derya Akan <d.akan369@gmail.com>
2024-01-11 21:09:15 +0300

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,9 +7,13 @@ import InstaImage3 from "../Assets/adv-aseets/insta/li-2.png";
 import InstaImage4 from "../Assets/adv-aseets/insta/li-3.png";
 import InstaImage5 from "../Assets/adv-aseets/insta/li-4.png";
 import InstaImage6 from "../Assets/adv-aseets/insta/li-5.png";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faEnvelopeOpenText,
+  faLocationDot,
+  faPhoneVolume,
+} from "@fortawesome/free-solid-svg-icons";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
 
 export default function Footer() {
   return (
@@ -20,10 +24,28 @@ export default function Footer() {
             <div className="footer-info-logo">
               <img src={FooterLogoSvg} />
               <div className="contact-footer">
-              <FontAwesomeIcon icon="fa-solid fa-location-dot" style={{color: "#FFD43B",}} />
+                <FontAwesomeIcon
+                  icon={faLocationDot}
+                  style={{ color: "#FDC913" }}
+                  size="1x"
+                />
+                <p>341 Londonderry Road, Istanbul Turkiye</p>
               </div>
-              <div className="contact-footer">B</div>
-              <div className="contact-footer">C</div>
+              <div className="contact-footer">
+                <FontAwesomeIcon
+                  icon={faEnvelopeOpenText}
+                  style={{ color: "#FDC913" }}
+                  size="1x"
+                />
+                <p>aciktim@teknolojikyemekler.com</p>
+              </div>
+              <div className="contact-footer">
+                <FontAwesomeIcon
+                  icon={faPhoneVolume}
+                  style={{ color: "#FDC913"}}
+                  size="1x"
+                /><p>+90 216 123 4567</p>
+              </div>
             </div>
             <div className="menu-footer">
               <h4>Sıccacık Yemekler</h4>
@@ -48,7 +70,14 @@ export default function Footer() {
           </div>
         </div>
         <hr />
-        <div className="copyright">blah blah</div>
+        <div className="copyright">
+            <p>@ 2023 Teknolojik Yemekler</p>
+            <FontAwesomeIcon
+                  icon={faTwitter}
+                  style={{ color: "white" }}
+                  size="1x"
+                />
+        </div>
       </footer>
     </>
   );
